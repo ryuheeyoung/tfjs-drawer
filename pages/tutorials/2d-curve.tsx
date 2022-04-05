c0a80837a75598be6dde128c11764460b45a76b7
ryuheeyoung <rhy901008@gmail.com>
2022-04-05 15:19:00 +0900

    #1. tf 데이터 컴포넌트 분리

--- pages/tutorials/2d-curve.tsx
+++ pages/tutorials/2d-curve.tsx
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
 
-import * as tf from "@tensorflow/tfjs";
-
 import {
   Chart as ChartJS,
   LinearScale,
@@ -10,12 +8,10 @@ import {
   LineElement,
   Tooltip,
   Legend,
-  ChartData,
   CategoryScale,
 } from "chart.js";
-import { History } from "@tensorflow/tfjs";
 import DataGraph from "../../components/tutorials/2d-curve/data-graph";
-import { Line } from "react-chartjs-2";
+import TfHistory from "../../components/tutorials/2d-curve/tf-history";
 
 ChartJS.register(
   LinearScale,
@@ -42,98 +38,6 @@ const dataUrl = "https://storage.googleapis.com/tfjs-tutorials/carsData.json";
 
 const TwoDCurvePage = () => {
   const [data, setData] = useState<Array<CarType>>();
-  const [hist, setHist] = useState<History>();
-
-  const [chartData, setChartData] = useState<ChartData<"line">>();
-
-  /**
-   * create sequence model
-   */
-  const createModel = (): tf.Sequential => {
-    // Create a sequential model
-    const model = tf.sequential();
-
-    // Add a single input layer
-    model.add(tf.layers.dense({ inputShape: [1], units: 1, useBias: true }));
-
-    // Add an output layer
-    model.add(tf.layers.dense({ units: 1, useBias: true }));
-
-    model.summary();
-    return model;
-  };
-
-  /**
-   * Convert the input data to tensors that we can use for machine
-   * learning. We will also do the important best practices of _shuffling_
-   * the data and _normalizing_ the data
-   * MPG on the y-axis.
-   */
-  const convertToTf = (data: Array<CarType>) => {
-    // Wrapping these calculations in a tidy will dispose any
-    // intermediate tensors.
-    return tf.tidy(() => {
-      // Step 1. Shuffle the data
-      tf.util.shuffle(data);
-
-      // Step 2. Convert data to Tensor
-      const inputs = data.map((d) => d.Horsepower);
-      const labels = data.map((d) => d.Miles_per_Gallon);
-
-      const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
-      const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
-
-      //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
-      const inputMax = inputTensor.max();
-      const inputMin = inputTensor.min();
-      const labelMax = labelTensor.max();
-      const labelMin = labelTensor.min();
-
-      const normalizedInputs = inputTensor
-        .sub(inputMin)
-        .div(inputMax.sub(inputMin));
-      const normalizedLabels = labelTensor
-        .sub(labelMin)
-        .div(labelMax.sub(labelMin));
-
-      return {
-        inputs: normalizedInputs,
-        labels: normalizedLabels,
-        // Return the min/max bounds so we can use them later.
-        inputMax,
-        inputMin,
-        labelMax,
-        labelMin,
-      };
-    });
-  };
-
-  const trainModel = async (
-    model: tf.Sequential,
-    inputs: tf.Tensor<tf.Rank>,
-    labels: tf.Tensor<tf.Rank>
-  ) => {
-    // Prepare the model for training.
-    model.compile({
-      optimizer: tf.train.adam(),
-      loss: tf.losses.meanSquaredError,
-      metrics: ["mse"],
-    });
-
-    const batchSize = 32;
-    const epochs = 50;
-
-    return await model.fit(inputs, labels, {
-      batchSize,
-      epochs,
-      shuffle: true,
-      // callbacks: tfvis.show.fitCallbacks(
-      //   { name: 'Training Performance' },
-      //   ['loss', 'mse'],
-      //   { height: 200, callbacks: ['onEpochEnd'] }
-      // )
-    });
-  };
 
   const getData = async () => {
     await fetch(dataUrl)
@@ -145,47 +49,10 @@ const TwoDCurvePage = () => {
       });
   };
 
-  const training = async () => {
-    console.log("@@@@@ summary");
-    const model = createModel();
-
-    console.log("@@@@@ Training Performance");
-    const tensorData = convertToTf(data);
-    const { inputs, labels } = tensorData;
-
-    const h = await trainModel(model, inputs, labels);
-
-    if (h) {
-      setHist(h);
-    }
-  };
-
   useEffect(() => {
     getData();
     console.log("Hello TensorFlow");
   }, []);
-
-  useEffect(() => {
-    if (data) {
-      training();
-    }
-  }, [data]);
-
-  useEffect(() => {
-    if (hist) {
-      const chData: ChartData<"line"> = {
-        labels: hist.epoch,
-        datasets: [
-          {
-            label: "epoch",
-            data: hist.history.mse as Array<number>,
-          },
-        ],
-      };
-
-      setChartData(chData);
-    }
-  }, [hist]);
 
   return (
     <>
@@ -210,10 +77,8 @@ const TwoDCurvePage = () => {
           background: "#ddd",
         }}
       >
-        {chartData && <Line data={chartData} />}
+        <TfHistory data={data} />
       </div>
-      <div>loss : {hist && hist.history.loss[0]}</div>
-      <div>mse : {hist && hist.history.mse[0]}</div>
     </>
   );
 };
